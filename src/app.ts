fd558568de3749a9236325b1871d178578492335
seungwonleee <seungwon.code@gmail.com>
2023-04-17 20:08:46 +0900

    Fix: api route add prefix api string

--- src/app.ts
+++ src/app.ts
@@ -54,9 +54,9 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hello World!');
 });
 
-app.use('/post', postRouter);
-app.use('/user', userRouter);
-app.use('/auth', authRouter);
+app.use('/api/post', postRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.use(errorHandler);
 
