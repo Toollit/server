55e36dd97f85996827153ab73ff419044d4ef8b5
seungwonleee <seungwon.code@gmail.com>
2023-05-25 04:13:09 +0900

    Fix: profile image entity column add

--- src/entity/ProfileImage.ts
+++ src/entity/ProfileImage.ts
@@ -1,10 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class ProfileImage {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ length: 500 })
+  @Column({ nullable: true, length: 500 })
   url: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn({ nullable: true, default: null })
+  updatedAt: Date | null = null;
 }
