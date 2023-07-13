4dfbe6167dc0aa4d2295ffdd7bb3035078060d54
seungwonleee <seungwon.code@gmail.com>
2023-07-14 06:29:34 +0900

    Add: profile page profile image update logic

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -16,6 +16,25 @@ import { User } from '@/entity/User';
 import { Project } from '@/entity/Project';
 import { Profile } from '@/entity/Profile';
 import { ProfileImage } from '@/entity/ProfileImage';
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import { S3Client } from '@aws-sdk/client-s3';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const S3_ACCESS_KEY_ID = process.env.S3_ACCESS_KEY_ID;
+const S3_SECRET_ACCESS_KEY = process.env.S3_SECRET_ACCESS_KEY;
+const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+const S3_BUCKET_REGION = process.env.S3_BUCKET_REGION;
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: S3_ACCESS_KEY_ID,
+    secretAccessKey: S3_SECRET_ACCESS_KEY,
+  },
+  region: S3_BUCKET_REGION,
+});
 
 const router = express.Router();
 
@@ -696,16 +715,48 @@ router.get(
   }
 );
 
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: S3_BUCKET_NAME,
+    key(req, file, callback) {
+      const userNickname = req.user?.nickname;
+      const newFileName = `${userNickname}-${new Date().getTime()}-${
+        file.originalname
+      }`;
+      const fullPath = `profileImage/${userNickname}/${newFileName}`;
+
+      callback(null, fullPath);
+    },
+  }),
+  limits: { fileSize: 10 * 1024 * 1204 }, // 10MB
+});
+
+const filterRequest = (req: Request, res: Response, next: NextFunction) => {
+  const { category } = req.params;
+
+  if (category !== 'profileImage') {
+    return next();
+  }
+
+  upload.single('profileImage')(req, res, next);
+};
+
+interface MulterRequest extends Request {
+  file: any;
+}
+
 // profile page. profile info update router
 router.post(
   '/profile/:category',
+  filterRequest,
+
   async (req: Request, res: Response, next: NextFunction) => {
     const user = req.user;
     const { category } = req.params;
     const { data } = req.body;
 
-    console.log({ category, data });
-
+    // console.log({ category, data });
     if (!user) {
       return res.status(400).json({
         success: false,
@@ -714,6 +765,27 @@ router.post(
     }
 
     try {
+      // update profile image
+      if (category === 'profileImage') {
+        const multerS3File = (req as MulterRequest).file;
+
+        const newProfileImageUrl = multerS3File.location;
+
+        await AppDataSource.createQueryBuilder()
+          .update(Profile)
+          .set({ profileImage: newProfileImageUrl })
+          .where('id = :id', { id: user?.id })
+          .execute();
+
+        return res.status(201).json({
+          success: true,
+          message: null,
+          data: {
+            url: multerS3File.location,
+          },
+        });
+      }
+
       // update profile nickname
       if (category === 'nickname') {
         const requestUser = user;
