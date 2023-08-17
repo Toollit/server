1f3d7d05b9cf240cd500dea5b7550a7a16f33db2
seungwonleee <seungwon.code@gmail.com>
2023-08-17 23:09:45 +0900

    Add: report entity

---
+++ src/entity/Report.ts
@@ -0,0 +1,46 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  UpdateDateColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Report {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  writerId: number;
+
+  @Column()
+  writerNickname: string;
+
+  @Column()
+  reporterId: number;
+
+  @Column()
+  reporterNickname: string;
+
+  @Column()
+  postType: string;
+
+  @Column()
+  postId: number;
+
+  @Column()
+  reason: string;
+
+  @Column()
+  resolved: 'pending' | 'fulfilled';
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn({ nullable: true, default: null })
+  updatedAt: Date | null = null;
+}
