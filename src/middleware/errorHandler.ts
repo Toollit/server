5b7d85096fc7c3837c7b5ad8b2d96215d31a87eb
seungwonleee <seungwon.code@gmail.com>
2023-11-24 08:01:32 +0900

    Refactor: errorHandler apply return keyword

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -3,7 +3,8 @@ import type { ErrorRequestHandler } from 'express';
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({
+
+  return res.status(500).json({
     success: false,
     message: SERVER_ERROR_DEFAULT,
   });
