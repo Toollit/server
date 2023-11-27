e1f94a320ca02ff7641162b7af9d0431edd4c87c
seungwonleee <seungwon.code@gmail.com>
2023-11-27 22:39:18 +0900

    Refactor: projects pagination router

--- src/routes/post/projects.ts
+++ src/routes/post/projects.ts
@@ -9,100 +9,112 @@ dotenv.config();
 
 const router = express.Router();
 
-// API to look up all projects. Using Pagination.
-router.get('/', async (req: Request, res: Response, next: NextFunction) => {
-  const page = Number(req.query.page);
-  const order = req.query.order as 'new' | 'popularity';
+interface ProjectReqQuery {
+  page: string;
+  order: 'new' | 'popularity';
+}
 
-  const postsPerPage = 12;
+// Look up all projects with using pagination router
+router.get(
+  '/',
+  async (
+    req: Request<{}, {}, {}, ProjectReqQuery>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const page = Number(req.query.page);
+    const order = req.query.order;
 
-  const skip = (page - 1) * postsPerPage;
+    const postsPerPage = 12;
 
-  const projectRepository = AppDataSource.getRepository(Project);
-  const bookmarkRepository = AppDataSource.getRepository(Bookmark);
+    const skip = (page - 1) * postsPerPage;
 
-  try {
-    const projects = await projectRepository.find({
-      relations: { hashtags: true, memberTypes: true, members: true },
-      order: order === 'new' ? { id: 'DESC' } : { views: 'DESC' },
-      skip: page >= 2 ? skip : 0,
-      take: postsPerPage,
-    });
+    const projectRepository = AppDataSource.getRepository(Project);
+    const bookmarkRepository = AppDataSource.getRepository(Bookmark);
 
-    const projectTotalCount = await projectRepository
-      .createQueryBuilder('projects')
-      .getCount();
+    try {
+      const projects = await projectRepository.find({
+        relations: { hashtags: true, memberTypes: true, members: true },
+        order: order === 'new' ? { id: 'DESC' } : { views: 'DESC' },
+        skip: page >= 2 ? skip : 0,
+        take: postsPerPage,
+      });
 
-    const totalPage = Math.ceil(projectTotalCount / postsPerPage);
+      const projectTotalCount = await projectRepository
+        .createQueryBuilder('projects')
+        .getCount();
 
-    const processedData = await Promise.all(
-      projects.map(async (project) => {
-        const extractTagNames = project.hashtags.map(
-          (hashtag) => hashtag.tagName
-        );
+      const totalPage = Math.ceil(projectTotalCount / postsPerPage);
 
-        const extractMemberTypes = project.memberTypes.map(
-          (memberType) => memberType.type
-        );
-
-        // developer, designer, pm, anyone 순으로 정렬
-        const orderedMemberTypes = extractMemberTypes.sort(function (a, b) {
-          return (
-            (a === 'developer'
-              ? -3
-              : a === 'designer'
-              ? -2
-              : a === 'pm'
-              ? -1
-              : a === 'anyone'
-              ? 0
-              : 1) -
-            (b === 'developer'
-              ? -3
-              : b === 'designer'
-              ? -2
-              : b === 'pm'
-              ? -1
-              : b === 'anyone'
-              ? 0
-              : 1)
+      const processedData = await Promise.all(
+        projects.map(async (project) => {
+          const extractTagNames = project.hashtags.map(
+            (hashtag) => hashtag.tagName
           );
-        });
 
-        const bookmarks = await bookmarkRepository.find({
-          where: {
-            projectId: project.id,
-          },
-        });
+          const extractMemberTypes = project.memberTypes.map(
+            (memberType) => memberType.type
+          );
 
-        const memberCount = project.members.length - 1; // Exclude project writer
+          // Sorts in order of developer, designer, pm, and anyone
+          const orderedMemberTypes = extractMemberTypes.sort(function (a, b) {
+            return (
+              (a === 'developer'
+                ? -3
+                : a === 'designer'
+                ? -2
+                : a === 'pm'
+                ? -1
+                : a === 'anyone'
+                ? 0
+                : 1) -
+              (b === 'developer'
+                ? -3
+                : b === 'designer'
+                ? -2
+                : b === 'pm'
+                ? -1
+                : b === 'anyone'
+                ? 0
+                : 1)
+            );
+          });
 
-        return {
-          id: project.id,
-          title: project.title,
-          views: project.views,
-          bookmarkCount: bookmarks.length,
-          hashtags: extractTagNames,
-          memberTypes: orderedMemberTypes,
-          memberCount,
-          recruitCount: project.recruitCount,
-          representativeImage: project.representativeImage,
-        };
-      })
-    );
+          const bookmarks = await bookmarkRepository.find({
+            where: {
+              projectId: project.id,
+            },
+          });
 
-    return res.status(200).json({
-      success: true,
-      message: null,
-      data: {
-        projects: processedData,
-        totalPage,
-      },
-    });
-  } catch (error) {
-    next(error);
+          const memberCount = project.members.length - 1; // -1 is to exclude project writer
+
+          return {
+            id: project.id,
+            title: project.title,
+            views: project.views,
+            bookmarkCount: bookmarks.length,
+            hashtags: extractTagNames,
+            memberTypes: orderedMemberTypes,
+            memberCount,
+            recruitCount: project.recruitCount,
+            representativeImage: project.representativeImage,
+          };
+        })
+      );
+
+      return res.status(200).json({
+        success: true,
+        message: null,
+        data: {
+          projects: processedData,
+          totalPage,
+        },
+      });
+    } catch (err) {
+      return next(err);
+    }
   }
-});
+);
 
 // API to check bookmark status of all posts
 router.get(
