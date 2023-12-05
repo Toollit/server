01cd0dda9796fc70a803cfeefa5fe0c72a905a32
seungwonleee <seungwon.code@gmail.com>
2023-12-05 19:04:27 +0900

    Add: logger middleware with morgan and winston

---
+++ src/middleware/logger.ts
@@ -0,0 +1,36 @@
+import morgan from 'morgan';
+import { winstonLogger } from './winston';
+
+const format = () => {
+  const result = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
+  return result;
+};
+
+// Forwarding morgan log information to winston logger via stream
+const stream = {
+  write: (message: string) => {
+    const removeAnsiEscapeCodesRegex =
+      /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
+
+    const statusCodeIndex = format() === 'combined' ? 8 : 2;
+    const statusCode = Number(
+      message.replace(removeAnsiEscapeCodesRegex, '').split(' ')[
+        statusCodeIndex
+      ]
+    );
+
+    if (statusCode >= 400) {
+      return winstonLogger.error(
+        message.replace(removeAnsiEscapeCodesRegex, '')
+      );
+    } else {
+      return winstonLogger.info(
+        message.replace(removeAnsiEscapeCodesRegex, '')
+      );
+    }
+  },
+};
+
+const logger = morgan(format(), { stream });
+
+export { logger };
