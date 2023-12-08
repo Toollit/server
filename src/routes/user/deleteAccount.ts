6104c4c2b553d325703b8379d9f2d2ae7ac49878
seungwonleee <seungwon.code@gmail.com>
2023-12-08 21:29:56 +0900

    Update: delete account router add sending service dropout confirm email

--- src/routes/user/deleteAccount.ts
+++ src/routes/user/deleteAccount.ts
@@ -1,14 +1,22 @@
 import { AppDataSource } from '@/data-source';
-import { Bookmark } from '@/entity/Bookmark';
-import { Profile } from '@/entity/Profile';
-import { User } from '@/entity/User';
 import { CLIENT_ERROR_ABNORMAL_ACCESS } from '@/message/error';
 import { isLoggedIn } from '@/middleware/loginCheck';
 import express, { NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
+import ejs from 'ejs';
+import path from 'path';
+import { v4 as uuidV4 } from 'uuid';
+import { DeleteAccountRequest } from '@/entity/deleteAccountRequest';
+
+dotenv.config();
+
+const ORIGIN_URL = process.env.ORIGIN_URL;
+const GETIT_LOGO_IMAGE_URL = process.env.GETIT_LOGO_IMAGE_URL;
 
 const router = express.Router();
 
-// Delete user account router
+// Sending delete user account confirmation email router
 router.post(
   '/',
   isLoggedIn,
@@ -21,63 +29,96 @@ router.post(
         message: CLIENT_ERROR_ABNORMAL_ACCESS,
       });
     }
+    const email = currentUser.email;
+    const a1 = uuidV4();
+    const a2 = uuidV4();
+    const a3 = uuidV4();
 
-    const queryRunner = AppDataSource.createQueryRunner();
+    const authCodeQuery = `?email=${email}&a1=${a1}&a2=${a2}&a3=${a3}`;
+    const deleteAccountConfirmPage = '/deleteAccount/confirm';
+    const deleteAccountConfirmURL =
+      ORIGIN_URL + deleteAccountConfirmPage + authCodeQuery;
 
     try {
-      await queryRunner.connect();
-      await queryRunner.startTransaction();
-
-      const user = await queryRunner.manager
-        .getRepository(User)
-        .createQueryBuilder('user')
-        .where('user.id = :id', { id: currentUser.id })
-        .leftJoinAndSelect('user.profile', 'profile')
+      const isExistDeleteAccountRequest = await AppDataSource.getRepository(
+        DeleteAccountRequest
+      )
+        .createQueryBuilder()
+        .where('email = :email', { email })
         .getOne();
 
-      if (!user) {
-        throw new Error('User does not exist.');
+      if (isExistDeleteAccountRequest) {
+        await AppDataSource.createQueryBuilder()
+          .update(DeleteAccountRequest)
+          .set({ email, a1, a2, a3 })
+          .where('email = :email', { email })
+          .execute();
       }
 
-      const profile = await queryRunner.manager
-        .getRepository(Profile)
-        .createQueryBuilder()
-        .where('id = :id', { id: user?.profile.id })
-        .getOne();
-
-      if (!profile) {
-        throw new Error('Profile does not exist.');
+      if (!isExistDeleteAccountRequest) {
+        await AppDataSource.createQueryBuilder()
+          .insert()
+          .into(DeleteAccountRequest)
+          .values({ email, a1, a2, a3 })
+          .execute();
       }
+    } catch (err) {
+      return next(err);
+    }
 
-      // Delete all user and related data except one to one relationship profile
-      // Cascade not working one to one relation.
-      // Github issue reference: https://github.com/typeorm/typeorm/issues/3218
-      await queryRunner.manager
-        .createQueryBuilder()
-        .delete()
-        .from(User)
-        .where('id = :id', { id: currentUser.id })
-        .execute();
+    let emailTemplate;
 
-      await queryRunner.manager
-        .createQueryBuilder()
-        .delete()
-        .from(Profile)
-        .where('id = :id', { id: profile.id })
-        .execute();
+    const appDir = path
+      .resolve(__dirname)
+      .replace('routes/user', '/template/deleteAccountConfirmMail.ejs');
 
-      await queryRunner.commitTransaction();
+    ejs.renderFile(
+      appDir,
+      {
+        getitLogo: GETIT_LOGO_IMAGE_URL,
+        getitURL: ORIGIN_URL,
+        deleteAccountConfirmURL,
+      },
+      function (err, data) {
+        if (err) {
+          return next(err);
+        }
+        emailTemplate = data;
+      }
+    );
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: `Getit <${process.env.NODEMAILER_USER}>`,
+      to: email,
+      subject: 'Getit 회원 탈퇴 확인 메일입니다.',
+      html: emailTemplate,
+    };
+
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (err) {
+        transporter.close();
+        return next(err);
+      }
+      console.log(
+        `[${info.accepted}] - finish sending email : ` + info.response
+      );
 
       return res.status(200).json({
         success: true,
         message: null,
       });
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-      return next(err);
-    } finally {
-      await queryRunner.release();
-    }
+    });
   }
 );
 
