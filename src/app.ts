c18d08dc6a079ecf581aef4256df620b30664df8
seungwonleee <seungwon.code@gmail.com>
2024-02-05 23:15:39 +0900

    Add: controls the maximum request body size

--- src/app.ts
+++ src/app.ts
@@ -43,8 +43,8 @@ app.use(helmet());
 app.use(compression());
 app.use(hpp());
 app.use(logger);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   session({
