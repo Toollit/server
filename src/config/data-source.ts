bb89bb3e5b45bd31264b8105a687c5e39074df07
seungwonleee <seungwon.code@gmail.com>
2024-03-11 05:53:28 +0900

    Refactor: typeorm data soure port settings

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -7,7 +7,7 @@ import mysql2 from 'mysql2';
 export const AppDataSource = new DataSource({
   type: 'mysql',
   host: process.env.DB_ENDPOINT,
-  port: Number(process.env.DB_PORT),
+  port: 3306,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
