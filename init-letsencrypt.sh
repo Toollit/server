1f081ba8eb4b5ec7af3e89288244c2be3cd7a4f4
seungwonleee <seungwon.code@gmail.com>
2024-03-21 06:44:37 +0900

    Fix: error: open() "/etc/letsencrypt/options-ssl-nginx.conf" failed (2: No such file or directory) in /etc/nginx/conf.d/default.conf:24
    - reference: https://eff-certbot.readthedocs.io/en/latest/install.html#alternative-1-docker

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(api.toollit.com)
 rsa_key_size=4096
-data_path="./nginx/certbot"
+data_path="/etc/letsencrypt"
 email="seungwon.code@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
