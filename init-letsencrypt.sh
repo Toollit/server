51724eecc2f2184de05415c49731da40b0a0028a
seungwonleee <seungwon.code@gmail.com>
2024-03-21 20:33:17 +0900

    Etc: debugging for init-letsencrypt.sh curl download

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -22,8 +22,14 @@ fi
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
   mkdir -p "$data_path/conf"
+  echo "@@@@@@@@@@ ====> path check"
+  pwd
   sudo curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  echo "@@@@@@@@@@ ====>  download options-ssl-nginx.conf check"
+  ls
   sudo curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  echo "@@@@@@@@@@ ====>  download ssl-dhparams.pem check"
+  ls
   echo
 fi
 
