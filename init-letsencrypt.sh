4caafaeae2855905497b56caca088ef281e5fc7f
seungwonleee <seungwon.code@gmail.com>
2024-03-25 23:38:54 +0900

    Docs: add comments to init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,8 +9,9 @@ domains="api.toollit.com"
 rsa_key_size=4096
 data_path="./data/certbot"
 email="seungwon.code@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # staging=1 for development, staging=0 for production. Set to 1 if you're testing your setup to avoid hitting request limits
 
+# If a folder exists in data_path during deployment, this script here indefinitely waits for a response so that the script below does not run.
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
