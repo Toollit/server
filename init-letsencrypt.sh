58535fd76b991136c6a16bdbb388f81e1799db9f
seungwonleee <seungwon.code@gmail.com>
2024-03-25 23:54:33 +0900

    Fix: to update new docker images

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -11,6 +11,16 @@ data_path="./data/certbot"
 email="seungwon.code@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # staging=1 for development, staging=0 for production. Set to 1 if you're testing your setup to avoid hitting request limits
 
+# To update new docker images
+echo "### Shutting down docker containers ..."
+docker compose -f docker-compose.prod.yml down
+echo
+
+# To update new docker images
+echo "### Run docker containers ..."
+docker compose -f docker-compose.prod.yml up -d
+echo
+
 # If a folder exists in data_path during deployment, this script here indefinitely waits for a response so that the script below does not run.
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
