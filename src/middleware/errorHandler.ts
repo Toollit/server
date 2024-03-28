d2e65f8e76805ee3348766a0a02c76990ac05725
seungwonleee <seungwon.code@gmail.com>
2024-03-28 09:02:59 +0900

    Refactor: error middleware error logging

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -2,7 +2,7 @@ import { SERVER_ERROR_DEFAULT } from '@/message/error';
 import type { ErrorRequestHandler } from 'express';
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.error(err.stack);
+  console.error(err);
 
   return res.status(500).json({
     success: false,
