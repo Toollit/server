a1aeea1332ec754dc487bb61b5dd468e342ea57f
seungwonleee <seungwon.code@gmail.com>
2024-04-09 23:43:32 +0900

    Update: typeorm dataSource aws parameter store request error handling

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -8,28 +8,32 @@ import { getParameterStore } from '@/utils/awsParameterStore';
 export let AppDataSource: Readonly<DataSource>;
 
 export const dataSource = (async () => {
-  const DB_ENDPOINT = await getParameterStore({ key: 'DB_ENDPOINT' });
-  const DB_USERNAME = await getParameterStore({ key: 'DB_USERNAME' });
-  const DB_PASSWORD = await getParameterStore({ key: 'DB_PASSWORD' });
-  const DB_DATABASE = await getParameterStore({ key: 'DB_DATABASE' });
+  try {
+    const DB_ENDPOINT = await getParameterStore({ key: 'DB_ENDPOINT' });
+    const DB_USERNAME = await getParameterStore({ key: 'DB_USERNAME' });
+    const DB_PASSWORD = await getParameterStore({ key: 'DB_PASSWORD' });
+    const DB_DATABASE = await getParameterStore({ key: 'DB_DATABASE' });
 
-  const source = new DataSource({
-    type: 'mysql',
-    host: DB_ENDPOINT,
-    port: 3306,
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    synchronize: false,
-    logging: true,
-    entities: [path.join(__dirname, '../entity/**/*.{ts,js}')],
-    migrations: [path.join(__dirname, '../migrations/*.{ts,js}')],
-    subscribers: [],
-    charset: 'utf8mb4_unicode_ci',
-    driver: mysql2,
-  });
+    const source = new DataSource({
+      type: 'mysql',
+      host: DB_ENDPOINT,
+      port: 3306,
+      username: DB_USERNAME,
+      password: DB_PASSWORD,
+      database: DB_DATABASE,
+      synchronize: false,
+      logging: true,
+      entities: [path.join(__dirname, '../entity/**/*.{ts,js}')],
+      migrations: [path.join(__dirname, '../migrations/*.{ts,js}')],
+      subscribers: [],
+      charset: 'utf8mb4_unicode_ci',
+      driver: mysql2,
+    });
 
-  AppDataSource = source;
+    AppDataSource = source;
 
-  return source;
+    return source;
+  } catch (err) {
+    console.error(err);
+  }
 })();
