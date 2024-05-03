049ec81092f8d059b163c213843f0bb491826da8
seungwonleee <seungwon.code@gmail.com>
2024-05-03 11:54:50 +0900

    Add: express custom response type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,15 @@
 import { UserData } from '@/types/user';
+import { Response } from 'express';
 
 declare global {
   namespace Express {
     interface User extends UserData {}
   }
 }
+interface ResponseJson {
+  success: boolean;
+  message: string | null;
+  data?: any;
+}
+
+export interface CustomResponse extends Response<ResponseJson> {}
