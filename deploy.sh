d76d23cb76f2e97594c7567cf96077898c31f342
seungwonleee <seungwon.code@gmail.com>
2024-05-03 17:20:46 +0900

    WIP: blue green deploy settings
    - fix health check error

--- deploy.sh
+++ deploy.sh
@@ -19,9 +19,6 @@ if [ $IS_GREEN ];then # 현재 실행중인 app이 green인 경우
   echo "2. blue container up"
   docker compose -f docker-compose.prod.yml up -d blue
 
-  echo "3. change nginx proxy_pass container"  
-  sed -i "s|proxy_pass http://.*:4001;|proxy_pass http://blue-container:4002;|" $NGINX_CONF
-
   while [ 1 = 1 ]; do
   echo "4. blue health check..."
   sleep 3
@@ -50,8 +47,6 @@ else
   echo "2. green container up"
   docker compose -f docker-compose.prod.yml up -d green
 
-  echo "3. change nginx proxy_pass container"
-  sed -i "s|proxy_pass http://.*:4002;|proxy_pass http://green-container:4001;|" $NGINX_CONF
 
   while [ 1 = 1 ]; do
     echo "4. green health check..."
