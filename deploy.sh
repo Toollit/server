cc47a7de2a030c39b57d81197de9ff692f692571
seungwonleee <seungwon.code@gmail.com>
2024-05-08 18:06:39 +0900

    Fix: deploy.sh deploy settings
    - docker compose command with docker-compose.prod.yml
    - new server health check

--- deploy.sh
+++ deploy.sh
@@ -3,44 +3,62 @@
 # image registry
 DOCKER_REPO=seungwoncode
 
-# current service check
+# check current service
 if docker ps | grep -q blue; then
     echo "Blue service is active."
     CURRENT_SERVICE="blue"
     NEW_SERVICE="green"
+    NEW_SERVICE_PORT=4002
 else
     echo "Green service is active."
     CURRENT_SERVICE="green"
     NEW_SERVICE="blue"
+    NEW_SERVICE_PORT=4001
 fi
 
-# modify nginx settings with new service (before reload)
+echo "modify nginx settings with new service (before reload)"
 NGINX_UPSTREAM="${NEW_SERVICE}_server"
 sed -i "s/proxy_pass http:\/\/.*_server;/proxy_pass http:\/\/$NGINX_UPSTREAM;/" ./nginx/default.conf
 
-# rebuild image with modified configuration and push it to the registry
+
+echo "push only modified information to the registry"
 docker build -t toollit-server-nginx ./nginx
 docker tag toollit-server-nginx:latest $DOCKER_REPO/toollit-server-nginx:latest
 docker push $DOCKER_REPO/toollit-server-nginx:latest
 
 
-# image updates and new service deployments
+echo "deploy new server (blue or green)"
 docker pull $DOCKER_REPO/toollit-server-app:latest
 docker pull $DOCKER_REPO/toollit-server-nginx:latest
-docker compose up -d $NEW_SERVICE
+docker compose -f docker-compose.prod.yml up -d certbot
+docker compose -f docker-compose.prod.yml up -d $NEW_SERVICE
 
-# start nginx service if nginx is not present
+
 if ! docker ps | grep -q nginx; then
-    docker compose up -d nginx
+    echo "start nginx service if nginx is not present"
+    sudo docker compose -f docker-compose.prod.yml up -d nginx
 fi
 
-# latency for service to be stable
-sleep 60
 
-# nginx setup reload and existing service down
+echo "new server health check"
+while [ 1 = 1 ]; do
+echo "health check..."
+sleep 3
+
+REQUEST=$(curl http://127.0.0.1:$NEW_SERVICE_PORT) # 새로운 container로 request
+
+if [ -n "$REQUEST" ]; then # 서비스 가능하면 health check 중지
+echo "health check success"
+break ;
+fi
+done;
+
+
+echo "nginx setup reload and existing service down"
 NGINX_CONTAINER=$(docker ps | grep "nginx" | awk '{print $1}')
 docker exec $NGINX_CONTAINER nginx -s reload
-docker compose stop $CURRENT_SERVICE
+sudo docker compose -f docker-compose.prod.yml stop $CURRENT_SERVICE
 
-# delete unused images
+
+echo "remove unused images"
 docker image prune -f
