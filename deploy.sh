c59631f24a1788fa4b604c613d10dd70446dd641
seungwonleee <seungwon.code@gmail.com>
2024-06-18 08:41:42 +0900

    Update: deploy.sh pulling update image and delete unused images

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,14 @@ if [ -z $IS_GREEN  ];then # blue가 실행중인 경우
 
   echo "### BLUE => GREEN ###"
 
-  echo "1. green container up"
+  echo "1. pulling update image"
+  docker compose -f docker-compose.prod.yml pull green
+
+  echo "2. green container up"
   docker compose -f docker-compose.prod.yml up -d green # green 컨테이너 실행
 
   while [ 1 = 1 ]; do
-  echo "2. green health check..."
+  echo "3. green health check..."
   sleep 3
 
   REQUEST=$(curl http://127.0.0.1:4002) # green으로 request
@@ -28,27 +31,33 @@ if [ -z $IS_GREEN  ];then # blue가 실행중인 경우
   fi
   done;
 
-  echo "3. update nginx settings with new service"
+  echo "4. update nginx settings with new service"
   NGINX_UPSTREAM="green:4000"
   sed -i "s/proxy_pass http:\/\/.*:4000;/proxy_pass http:\/\/$NGINX_UPSTREAM;/" ./nginx/default.conf
 
-  echo "4. reload nginx"
+  echo "5. reload nginx"
   docker cp ./nginx/default.conf nginx:/etc/nginx/conf.d/default.conf
   sleep 5 # brief delay to ensure the configuration is fully updated
   docker compose -f docker-compose.prod.yml exec nginx nginx -s reload
 
-  echo "5. blue container down"
+  echo "6. blue container down"
   docker compose -f docker-compose.prod.yml down blue
+
+  echo "7. delete unused images"
+  sudo docker image prune -a -f
 
 else
 
   echo "### GREEN => BLUE ###"
 
-  echo "1. blue container up"
+  echo "1. pulling update image"
+  docker compose -f docker-compose.prod.yml pull blue
+
+  echo "2. blue container up"
   docker compose -f docker-compose.prod.yml up -d blue # blue 컨테이너 실행
 
   while [ 1 = 1 ]; do
-  echo "2. blue health check..."
+  echo "3. blue health check..."
   sleep 3
   
   REQUEST=$(curl http://127.0.0.1:4001) # blue로 request
@@ -59,15 +68,19 @@ else
   fi
   done;
 
-  echo "3. update nginx settings with new service"
+  echo "4. update nginx settings with new service"
   NGINX_UPSTREAM="blue:4000"
   sed -i "s/proxy_pass http:\/\/.*:4000;/proxy_pass http:\/\/$NGINX_UPSTREAM;/" ./nginx/default.conf
 
-  echo "4. reload nginx" 
+  echo "5. reload nginx" 
   docker cp ./nginx/default.conf nginx:/etc/nginx/conf.d/default.conf
   sleep 5 # brief delay to ensure the configuration is fully updated
   docker compose -f docker-compose.prod.yml exec nginx nginx -s reload
 
-  echo "5. green container down"
+  echo "6. green container down"
   docker compose -f docker-compose.prod.yml down green
+
+  echo "7. delete unused images"
+  sudo docker image prune -a -f
+
 fi
