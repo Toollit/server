cb62461ffba0dbbc1e2ee7a35e8b640bef89d8a5
seungwonleee <seungwon.code@gmail.com>
2024-08-01 23:47:34 +0900

    Refactor: intuitively modify the path of the router

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -5,20 +5,20 @@ import duplicateNicknameCheck from './duplicateNicknameCheck';
 import signin from './signin';
 import logout from './logout';
 import profile from './profile';
-import pwInquiry from './pwInquiry';
-import resetPassword from './resetPassword';
+import temporaryPassword from './temporaryPassword';
+import updatePassword from './updatePassword';
 import signup from './signup';
 
 const router = express.Router();
 
 router.use('/contact', contact);
-router.use('/deleteAccount', deleteAccount);
+router.use('/delete-account', deleteAccount);
 router.use('/duplicate-nickname-check', duplicateNicknameCheck);
 router.use('/signin', signin);
 router.use('/logout', logout);
 router.use('/profile', profile);
-router.use('/pw-inquiry', pwInquiry);
-router.use('/reset-password', resetPassword);
+router.use('/temporary-password', temporaryPassword);
+router.use('/update-password', updatePassword);
 router.use('/signup', signup);
 
 export default router;
