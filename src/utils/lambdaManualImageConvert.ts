531773cbfe8992eb88876281e6ca0335a6b8bd62
seungwonleee <seungwon.code@gmail.com>
2024-09-27 22:44:19 +0900

    Update: lambdaManualImageConvert util function return converted image url received from aws

--- src/utils/lambdaManualImageConvert.ts
+++ src/utils/lambdaManualImageConvert.ts
@@ -4,11 +4,12 @@ import {
   InvokeCommandInput,
 } from '@aws-sdk/client-lambda';
 import { getParameterStore } from '@/utils/awsParameterStore';
+import { TextDecoder } from 'util';
 
 /**
  * Function to manually call Lambda to resize and change format the image.
  * @param s3ObjectKey - S3 path of the original source image file.
- * @returns void
+ * @returns Image url received from aws
  */
 export const lambdaManualImageConvert = async (s3ObjectKey: string) => {
   try {
@@ -30,9 +31,12 @@ export const lambdaManualImageConvert = async (s3ObjectKey: string) => {
 
     const data = await client.send(command);
 
-    console.log('Lambda 호출 성공:', data.Payload);
+    const convertedImageUrl = JSON.parse(
+      new TextDecoder().decode(data.Payload)
+    );
+    console.log('Lambda 호출 성공:', convertedImageUrl);
 
-    return;
+    return convertedImageUrl;
   } catch (err) {
     console.error('Lambda 호출 실패:', err);
   }
